918c3b6e36d7d3a1a43048fab2be4c8f47817143
Alex Zabiela <alex.zabiela@and.digital>
2024-01-14 15:36:58 +0000

    add dropdown for customer on order create form, slight style changes

--- order-system-frontend/src/components/OrderForm.js
+++ order-system-frontend/src/components/OrderForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
@@ -15,6 +15,23 @@ function OrderForm() {
     startDate: '',
   });
 
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const api = axios.create({
+      baseURL: 'http://localhost:3001',
+    });
+
+    // Fetch customers
+    api.get('/api/customers')
+      .then((response) => {
+        setCustomers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching customers:', error);
+      });
+  }, []);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setOrder({ ...order, [name]: value });
@@ -23,13 +40,13 @@ function OrderForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const api = axios.create({
-      baseURL: 'http://localhost:3001', // Update the base URL to match your backend
+      baseURL: 'http://localhost:3001',
     });
 
-    api
-      .post('/api/orders', order)
+    api.post('/api/orders', order)
       .then((response) => {
         console.log('Order created:', response.data);
+        // Reset form after successful submission
         setOrder({
           orderName: '',
           status: '',
@@ -51,6 +68,17 @@ function OrderForm() {
     <div className='container'>
       <h2>Create Order</h2>
       <form onSubmit={handleSubmit}>
+      <div>
+          <label>Customer Code:</label>
+          <select name="customerCode" value={order.customerCode} onChange={handleChange}>
+            <option value="">Select a Customer</option>
+            {customers.map((customer) => (
+              <option key={customer.id} value={customer.companyCode}>
+                {customer.companyName} ({customer.companyCode})
+              </option>
+            ))}
+          </select>
+        </div>
         <div>
           <label>Order Name:</label>
           <input type="text" name="orderName" value={order.orderName} onChange={handleChange} />
@@ -79,15 +107,6 @@ function OrderForm() {
         <div>
           <label>Sales Order:</label>
           <input type="text" name="salesOrder" value={order.salesOrder} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Customer Code:</label>
-          <input
-            type="text"
-            name="customerCode"
-            value={order.customerCode}
-            onChange={handleChange}
-          />
         </div>
         <div>
           <label>Deadline:</label>
